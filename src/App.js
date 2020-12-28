07038a7ee704eb9f2e5ee237d2ff9c481981b1e7
Hydra <hydra@phenomenaltech.com>
2020-12-28 16:38:15 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-function App() {
-  return (
-    <div>
-      {/* Setup Router here */}
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import './index.css';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Login from './account/login';
+import Signup from './account/signup';
+import Employeelogin from './account/employeelogin';
 
-export default App;
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+       
+        
+          <Switch>
+                <Route exact path= "/" render={() => (
+                  <Redirect to="/login"/>
+                )}/>
+                 <Route exact path='/login' component={Login} />
+                 <Route exact path='/signup' component={Signup} />
+                 <Route exact path='/employeelogin' component={Employeelogin} />
+          </Switch>
+      
+    </Router>
+      </div>
+    )
+  }
+}
