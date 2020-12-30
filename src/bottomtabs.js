9d8216f979562eb4327af93d3f75ce535c2716e9
Hydra <hydra@phenomenaltech.com>
2020-12-30 19:01:31 +0530

    ok

--- src/bottomtabs.js
+++ src/bottomtabs.js
@@ -1,29 +1,10 @@
 import React from 'react'
-import { Button, WhiteSpace,WingBlank } from 'antd-mobile';
+
 
 export default function bottomtabs() {
     return (
         <div >
-            <WingBlank>
-    <Button>default</Button><WhiteSpace />
-    <Button disabled>default disabled</Button><WhiteSpace />
-
-    <Button type="primary">primary</Button><WhiteSpace />
-    <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
-
-    <Button type="warning">warning</Button><WhiteSpace />
-    <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
-
-    <Button loading>loading button</Button><WhiteSpace />
-    <Button icon="check-circle-o">with icon</Button><WhiteSpace />
-    <Button icon={<img src="https://gw.alipayobjects.com/zos/rmsportal/jBfVSpDwPbitsABtDDlB.svg" alt="" />}>with custom icon</Button><WhiteSpace />
-    <Button icon="check-circle-o" inline size="small" style={{ marginRight: '4px' }}>with icon and inline</Button>
-    <Button icon="check-circle-o" inline size="small">with icon and inline</Button>
-    <WhiteSpace />
-
-   
-    <WhiteSpace />
-    </WingBlank>
+            
         </div>
     )
 }
