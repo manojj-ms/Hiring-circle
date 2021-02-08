2c9bb9d307ed08d438e993f49e61c8e22509ed3d
Manoj <Manoj Kumar>
2021-02-08 19:20:36 +0530

    Attendance

--- src/views/employer/attendance/Employeeattendance.js
+++ src/views/employer/attendance/Employeeattendance.js
@@ -6,9 +6,9 @@ const Attendance = () => {
         <p className="text-center font-extrabold text-2xl pt-4">Attendance</p>
 
         <div className="mt-4">
-      <label class="block">
-  <span class="text-base">Employees List</span>
-  <select class="form-select bg-gray-200 block w-full mt-3 h-9 rounded-lg">
+      <label className="block">
+  <span className="text-base">Employees List</span>
+  <select className="form-select bg-gray-200 block w-full mt-3 h-9 rounded-lg">
     <option>Armstrong</option>
     <option>Bala</option>
     <option>Charles</option>
@@ -17,18 +17,18 @@ const Attendance = () => {
 </div>
 
      <div className="mt-4">
-    <div class="flex flex-col">
-                <label class="leading-loose">Date</label>
-                <div class="relative focus-within:text-gray-600 text-gray-400">
-                  <input type="text" class="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md bg-gray-200 focus:outline-none text-gray-600" placeholder="25/02/2020"/>
-                  <div class="absolute left-3 top-2">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+    <div className="flex flex-col">
+                <label className="leading-loose">Date</label>
+                <div className="relative focus-within:text-gray-600 text-gray-400">
+                  <input type="text" className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md bg-gray-200 focus:outline-none text-gray-600" placeholder="25/02/2020"/>
+                  <div className="absolute left-3 top-2">
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                   </div>
                 </div>
               </div>
      </div>
 
-     <button class=" px-4 justify-center mt-6 bg-blue-700 text-white border border-blue-700 font-bold py-2 w-full rounded-lg">
+     <button className=" px-4 justify-center mt-6 bg-blue-700 text-white border border-blue-700 font-bold py-2 w-full rounded-lg">
   <Link to='/employer/employeeslist'>Get Employee List</Link>
 </button>
 
--- src/views/employer/attendance/employeeslist.js
+++ src/views/employer/attendance/employeeslist.js
@@ -6,30 +6,30 @@ const EmployeeList = () => {
                 <p className="text-center font-extrabold text-2xl pt-4">Employee List</p>
 
                 <div className="mt-4 px-4">
-      <label class="block">
-  <span class="text-base">Date</span>
-  <div class="form-input select-text bg-gray-200 mt-3 block w-40 h-9 rounded-lg text-center text-2xl">12-01-2021</div>
+      <label className="block">
+  <span className="text-base">Date</span>
+  <div className="form-input select-text bg-gray-200 mt-3 block w-40 h-9 rounded-lg text-center text-2xl">12-01-2021</div>
 </label>
 </div>
 
  <div className="mt-4 px-4">
-      <label class="block">
-  <span class="text-base">Time-Start</span>
-  <div class="form-input select-text bg-gray-200 mt-3 block w-40 h-9 rounded-lg text-center text-2xl">09:00AM</div>
+      <label className="block">
+  <span className="text-base">Time-Start</span>
+  <div className="form-input select-text bg-gray-200 mt-3 block w-40 h-9 rounded-lg text-center text-2xl">09:00AM</div>
 </label>
 </div>
 
 <div className="mt-4 px-4">
-      <label class="block">
-  <span class="text-base">Time-End</span>
-  <div class="form-input select-text bg-gray-200 mt-3 block w-40 h-9 rounded-lg text-center text-2xl">07:00PM</div>
+      <label className="block">
+  <span className="text-base">Time-End</span>
+  <div className="form-input select-text bg-gray-200 mt-3 block w-40 h-9 rounded-lg text-center text-2xl">07:00PM</div>
 </label>
 </div>
 
 <div className="mt-4 px-4">
-      <label class="block">
-  <span class="text-base">Status</span>
-  <div class="form-input select-text bg-gray-200 mt-3 block w-40 h-9 rounded-lg text-center text-2xl">PRESENT</div>
+      <label className="block">
+  <span className="text-base">Status</span>
+  <div className="form-input select-text bg-gray-200 mt-3 block w-40 h-9 rounded-lg text-center text-2xl">PRESENT</div>
 </label>
 </div>
         </div>
